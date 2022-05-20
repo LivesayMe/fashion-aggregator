2996fbdf618f7268b1da0d11da2dff4afd555c0c
Hunter Livesay <livesayme@gmail.com>
2022-05-20 16:53:23 -0600

    Adds documentation to the api-docs route

--- swagger.js
+++ swagger.js
@@ -14,3 +14,77 @@ const endpointsFiles = ['./routes.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
+
+const userSchema = 
+{
+    "type": "object",
+    "properties": {
+        "username": {
+            "type": "string",
+            "description": "The username of the user"
+        },
+        "email": {
+            "type": "string",
+            "description": "The email of the user"
+        },
+        "favorites": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "description": "The IDs of the favorite outfits of the user"
+        },
+        "ratings": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "description": "The IDs of the rated outfits of the user"
+        }
+    },
+    "required": ["username", "email", "favorites", "ratings"]
+};
+
+const outfitSchema = 
+{
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string",
+            "description": "The name of the outfit"
+        },
+        "description": {
+            "type": "string",
+            "description": "The description of the outfit"
+        },
+        "images": {
+            "type": "array",
+            "items": {
+                "type": "string"
+            },
+            "description": "IDs of the images of the outfit"
+        }
+    },
+    "required": ["name", "description", "images"]
+};
+
+const ratingSchema = 
+{
+    "type": "object",
+    "properties": {
+        "userId": {
+            "type": "string",
+            "description": "The ID of the user who rated the outfit"
+        },
+        "outfitId": {
+            "type": "string",
+            "description": "The ID of the outfit that was rated"
+        },
+        "rating": {
+            "type": "number",
+            "description": "The rating of the outfit"
+        }
+    },
+    "required": ["userId", "outfitId", "rating"]
+};
